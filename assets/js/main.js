1c1243d151fd9de9cb909fd2f8cea09de9c3fab9
basketking <nemethhaz@gmail.com>
2022-04-05 13:40:38 +0200

    Slight changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -147,21 +147,27 @@ btnbg.addEventListener("click", changeBackground);
 // Get the modal
 var modal = document.getElementById("myModal");
 
+
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
 
+
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
+
 
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
   modal.style.display = "block";
 }
 
+
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
 }
+
+
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
@@ -170,17 +176,13 @@ window.onclick = function(event) {
   }
 }
 
+
 // When the user presses ESC, the modal closes
 $(document).keydown(function(event) { 
     if (event.keyCode == 27) { 
       modal.style.display = "none";
     }
   });
-
-
-
-
-
 
 
 
